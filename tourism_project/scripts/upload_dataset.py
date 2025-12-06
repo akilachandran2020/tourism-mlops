6389ea870c979fbde7bbaacdf9c60dd3dca012ef
akilachandran2020 <akilachandran2020@gmail.com>
2025-12-06 20:16:15 +0530

    Update upload_dataset.py

--- tourism_project/scripts/upload_dataset.py
+++ tourism_project/scripts/upload_dataset.py
@@ -1,12 +1,22 @@
-from huggingface_hub import HfApi, upload_folder
 import os
+from huggingface_hub import HfApi, upload_folder
 
-api = HfApi()
+repo_id = "AkilaChandran2025/tourism_dataset"   # change if needed
 
-repo_id = "AkilaChandran2025/tourism"  # change if needed
+# Create dataset folder
+os.makedirs("dataset", exist_ok=True)
 
+# Copy dataset files into dataset/
+if os.path.exists("tourism_project/data/tourism.csv"):
+    os.system("cp tourism_project/data/tourism.csv dataset/")
+if os.path.exists("tourism_project/data/train.csv"):
+    os.system("cp tourism_project/data/train.csv dataset/")
+if os.path.exists("tourism_project/data/test.csv"):
+    os.system("cp tourism_project/data/test.csv dataset/")
+
+# Upload dataset folder
 upload_folder(
-    folder_path="./test.csv",  # where train.csv/test.csv are
+    folder_path="dataset",
     repo_id=repo_id,
     repo_type="dataset"
 )
