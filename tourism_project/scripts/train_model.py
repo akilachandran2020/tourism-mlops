c2a73c472c1489224d6eda8b65ff7718ce8c2695
akilachandran2020 <akilachandran2020@gmail.com>
2025-12-06 21:52:22 +0530

    Update train_model.py

--- tourism_project/scripts/train_model.py
+++ tourism_project/scripts/train_model.py
@@ -1,25 +1,14 @@
 import pandas as pd
-import os
-from xgboost import XGBRegressor
 import joblib
+from xgboost import XGBRegressor
 import mlflow
+import os
 
-print("Starting model training...")
+TRAIN_PATH = "tourism_project/data/train.csv"
 
-TRAIN_CLEAN_PATH = "tourism_project/data/processed/train_clean.csv"
+train = pd.read_csv(TRAIN_PATH)
 
-print("🔎 Checking if cleaned train file exists:")
-print("    Exists:", os.path.exists(TRAIN_CLEAN_PATH))
-
-if not os.path.exists(TRAIN_CLEAN_PATH):
-    print("ERROR: File NOT FOUND! Here are all files in data/processed:")
-    os.system("ls -R tourism_project/data/processed")
-    raise FileNotFoundError(TRAIN_CLEAN_PATH)
-
-train = pd.read_csv(TRAIN_CLEAN_PATH)
-
-TARGET_COLUMN = "target"  # update if needed
-print("Columns in training data:", train.columns)
+TARGET_COLUMN = "target"   # change if needed
 
 X = train.drop(TARGET_COLUMN, axis=1)
 y = train[TARGET_COLUMN]
@@ -33,4 +22,6 @@ os.makedirs("tourism_project/models", exist_ok=True)
 MODEL_PATH = "tourism_project/models/xgboost_model.joblib"
 joblib.dump(model, MODEL_PATH)
 
-print("Training completed! Model saved to:", MODEL_PATH)
+mlflow.log_artifact(MODEL_PATH)
+
+print("Model training completed successfully!")
